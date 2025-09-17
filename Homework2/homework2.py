f26d0f3f5f7e77981cccdf726c80b027daacecff
Jane <nocubesp@gmail.com>
2025-09-17 16:45:44 +0200

    Bug fix

--- Homework2/homework2.py
+++ Homework2/homework2.py
@@ -17,39 +17,39 @@ def parse_fasta(filename):
                 sequences.append(line[:-1])
     return ''.join(sequences)
 
-letters_to_num = {'A': 1, 'C': 2, 'T': 4, 'G': 3}
-letters_to_revnum = {'A': 4, 'C': 3, 'T': 1, 'G': 2}
+letters_to_num = {'A': 1, 'C': 2, 'T': 4, 'G': 3, 'N': 0}
+letters_to_revnum = {'A': 4, 'C': 3, 'T': 1, 'G': 2, 'N':0}
 
 def calculate_20mer_set(inputs):
+    
     counts = set()
     current_read = 0
     current_rev_read = 0
-    mod = (4**30)+1
-    constant = 4**29
-    for i in range(30):
-        current_read = 4*current_read + letters_to_num[inputs[i]]
-        current_rev_read = 4*current_rev_read + letters_to_revnum[inputs[29-i]]
-    counts.add(min(current_read, current_rev_read))
+    constant = 5**19
+    for i in range(20):
+        current_read = 5*current_read + letters_to_num[inputs[i]]
+        current_rev_read = 5*current_rev_read + letters_to_revnum[inputs[19-i]]
+    counts.add(current_read ) # min(current_read, current_rev_read))
 
-    for i in range(31, len(inputs)):
+    for i in range(21, len(inputs)):
         if(inputs[i]!="N" and inputs[i-20]!="N"):
-            current_read = 4*(current_read % mod) + letters_to_num[inputs[i]]
+            current_read = 5*(current_read % constant) + letters_to_num[inputs[i]]
             current_rev_read = current_rev_read - letters_to_revnum[inputs[i-20]] + letters_to_revnum[inputs[i]]*constant
-            counts.add(min(current_read, current_rev_read))
+            counts.add(current_read) #min(current_read, current_rev_read))
     return counts
 
 def jaccard_index(set1,  set2):
-    return len(set1 & set2) / len(set1 | set2)
+    return float(len(set1 & set2)) / float(len(set1 | set2))
 
 def kmer_counting(inputs):
     counts = [Counter() for i in range(29)]
     current_read = [0] * 29
     for i in range(len(inputs)):
         for j in range(min(30, i+1),2, -1):
-            current_read[j-2] = current_read[j-3]*4 + letters_to_num[inputs[i]]
+            current_read[j-2] = current_read[j-3]*5 + letters_to_num[inputs[i]]
             counts[j-2][current_read[j-2]]+=1
         if(i>0):
-            current_read[0] = 4*letters_to_num[inputs[i-1]] + letters_to_num[inputs[i]]
+            current_read[0] = 5*letters_to_num[inputs[i-1]] + letters_to_num[inputs[i]]
             counts[0][current_read[0]]+=1
     return [len(a) for a in counts]
 
@@ -65,7 +65,7 @@ def main():
     for i in range(1,7):
         for j in range(i+1, 7):
             print("     -> Index between sequences " + str(i) + " and " + str(j) + " : " + str(jaccard_index(kmerseqs[i-1], kmerseqs[j-1])))
-    #All indexes are 0 so very similar kmer representation
+    # 2 and 5 are very similar. 4 and 6 have some resemblance
     x = [i for i in range(2,31)]
     y = kmer_counting(seqs[2])
     plt.plot(x, y, marker='o', linestyle='-', label = "Real Genome")
